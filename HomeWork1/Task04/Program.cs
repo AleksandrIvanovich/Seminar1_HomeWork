97c305d9e1063b6748fa723c1facf83bff1880be
Aleksandr <fomichov73@mail.ru>
2022-09-19 00:17:36 +0300

    Added Task04

---
+++ HomeWork1/Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿ // Напишите программу, которая на вход принимает число (N), 
+ // а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число больше 1:");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1)
+{
+    Console.WriteLine("Введено некорректное число!");
+}
+else
+{
+    for (int i = 2; i <= num; i = i + 2)
+    Console.Write($"{i} ");
+    Console.WriteLine(" ");
+}
